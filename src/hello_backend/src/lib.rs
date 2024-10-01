cdabd804c2e825787be8bd7e53008a43cf5faec9
Hacken <taokeqin@gmail.com>
2024-10-02 00:19:10 +0800

    only allow one account to interaction

--- src/hello_backend/src/lib.rs
+++ src/hello_backend/src/lib.rs
@@ -1,13 +1,20 @@
+use ic_cdk::caller;
 use ic_cdk_macros::*;
 use std::cell::RefCell;
 use std::collections::HashMap;
-
+const AUTHORIZED_PRINCIPAL: &str = "a33qp-z47xc-7tlpb-zvnd5-ykidh-tf4uk-xcbtp-2jucw-phkiq-hnclv-5ae";
 thread_local! {
     static STORE: RefCell<HashMap<String, String>> = RefCell::new(HashMap::new());
 }
 
 #[update]
 fn record_heartbeat(subject: String, time: String) {
+    let caller = caller();
+    assert_eq!(
+        caller.to_text(),
+        AUTHORIZED_PRINCIPAL,
+        "Caller is not authorized to record heartbeats"
+    );
     STORE.with(|store| {
         store.borrow_mut().insert(subject, time);
     });
